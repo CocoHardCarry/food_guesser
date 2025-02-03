e842fb078b3144610801740f006368756bec3f98
EmpresCoco <jaydenhang08@gmail.com>
2025-02-02 20:30:08 -0800

    changed prediction

--- server.py
+++ server.py
@@ -1,52 +1,35 @@
 import streamlit as st
 from fastai.vision.all import *
 
-def extract_last_folder_name(path):
-   parts = str(path).split("/")
-   return parts[5]
 
-model_path = "food_prediction_model.pkl"
+def extract_last_folder_name(path):
+    parts = str(path).split("/")
+    return parts[5]
+
+model_path = Path("food_prediction_model.pkl")
 food_predict_model = load_learner(model_path)
 
-
 def predict_food(image):
-   img = PILImage.create(image)  # Use PILImage.create to open the image
+    img = PILImage.create(image)
 
+    resized = img.resize((224, 224))
 
-   resized = img.resize((224, 224))
+    prediction = food_predict_model.predict(resized)
 
+    predicted_class = prediction[0]
+    breed_index = prediction[1].item()
+    accuracy = prediction[2][breed_index]
 
-   prediction = food_predict_model.predict(resized)
+    return predicted_class, accuracy
 
-
-   breed_index = prediction[1].item()
-
-
-   accuracy = prediction[2][breed_index]
-
-
-
-
-   return prediction, accuracy
-
-
-sample_dir = "/kaggle/input/fruit-and-vegetable-image-recognition/test"
-
-
-sample_files = get_image_files(sample_dir)
-
-
-# for i in range(10):
-   # predict_food(sample_files[i])
-
-
-st.text("Food Prediction Model")
+# Streamlit app UI
+st.title("Food Prediction Model")
 st.text("Built by Jayden Hang")
 
+# File uploader for user to upload an image
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
 
-uploaded_file = st.file_uploader("Choose an image...", type=["jpg","png","jpeg"])
-
-
+# If a file is uploaded, make a prediction
 if uploaded_file is not None:
     prediction_class, accuracy = predict_food(uploaded_file)
-    st.image(uploaded_file, caption=f"Prediction: {prediction_class} - {(accuracy * 100):.2f}%", use_column_width=True)
+    st.image(uploaded_file, caption=f"Prediction: {predicted_class} - {(accuracy * 100):.2f}%", use_column_width=True)
