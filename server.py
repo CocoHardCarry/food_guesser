ed54b62c7aa33699360f4490fab6de6c6b6a0dbc
EmpresCoco <jaydenhang08@gmail.com>
2025-02-08 14:26:07 -0800

    changed prediction

--- server.py
+++ server.py
@@ -35,5 +35,5 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg
 # If a file is uploaded, make a prediction
 if uploaded_file is not None:
     prediction_class = predict_food(uploaded_file)
-    st.title(uploaded_file, caption="",use_column_width=True)
+    st.title(uploaded_file)
 
